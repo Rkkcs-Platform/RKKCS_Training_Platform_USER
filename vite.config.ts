34f66a33eeec9145aead0103869f39dfd42a5c53
longlv <luongttxdks@gmail.com>
2026-06-07 16:10:34 +0700

    base user

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    tailwindcss(),
+  ],
+})
