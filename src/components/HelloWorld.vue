89c7566baf4e994041146cf54e2234249fd353b4
longlv <luongttxdks@gmail.com>
2026-06-10 20:47:25 +0700

    fix deployment

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import viteLogo from '../assets/vite.svg'
-import heroImg from '../assets/hero.png'
 import vueLogo from '../assets/vue.svg'
 
 const count = ref(0)
+const isActive = ref(true)
 </script>
 
 <template>
@@ -20,7 +20,7 @@ const count = ref(0)
       </span>
     </div>
     <div class="hero">
-      <img :src="heroImg" class="base" width="170" height="179" alt="" />
+      <img src="/favicon.svg" class="base" width="170" height="179" alt="" />
       <img :src="vueLogo" class="framework" alt="Vue logo" />
       <img :src="viteLogo" class="vite" alt="Vite logo" />
     </div>
